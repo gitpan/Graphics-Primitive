b76e5b69b22d66e83fc145d1e49b9f8bc8fc188b
Cory G Watson <gphat@cpan.org>
2009-05-17 23:26:03 +0000

    Import of GPHAT/Graphics-Primitive-0.43 from CPAN.
    gitpan-cpan-distribution: Graphics-Primitive
    gitpan-cpan-version:      0.43
    gitpan-cpan-path:         GPHAT/Graphics-Primitive-0.43.tar.gz
    gitpan-cpan-author:       GPHAT
    gitpan-cpan-maturity:     released

--- lib/Graphics/Primitive/Container.pm
+++ lib/Graphics/Primitive/Container.pm
@@ -23,7 +23,7 @@ has 'layout_manager' => (
     is => 'rw',
     isa => 'Layout::Manager',
     handles => [ 'do_layout' ],
-    trigger => sub { my ($self) = @_; $self->prepared(0); }
+    trigger => sub { my ($self) = @_; $self->prepared(0); },
 );
 
 sub add_component {
